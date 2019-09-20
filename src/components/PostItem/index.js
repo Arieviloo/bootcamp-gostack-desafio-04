5921457fd02168b2eab97456991f4a845c58e320
Arievilo <jadieoliveira00@gmail.com>
2019-09-19 22:47:54 -0300

    add comentários

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css';
 
-const PostItem = ({ author, date }) => {
+const PostItem = ({ author, date, content }) => {
   return (
     <div className="postTop">
       <img className="avatar" src={author.avatar} />
@@ -9,8 +9,36 @@ const PostItem = ({ author, date }) => {
         <span>{author.name}</span>
         <span>{date}</span>
       </div>
+      <div className="firstCommit">
+        <p>{content}</p>
+      </div>
     </div>
   );
 };
 
-export default PostItem;
+// function PostComments({ comments }) {
+//   return (
+//     <div className="post-comments">
+//       <div className="divider" />
+//       {comments.map(comment => (
+//         <div key={comment.id} className="comment">
+//           <img className="avatar" src={comment.author.avatar} />
+//           <p>
+//             <span>{comment.author.name}</span>
+//             {comment.content}
+//           </p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+function Post({ author, date, content }) {
+  return (
+    <div className="post">
+      <PostItem author={author} date={date} content={content} />
+    </div>
+  );
+}
+
+export default Post;
